339d96e81d33466cff924a0c87818d13e6f24436
Herin Chung <lync2846@gmail.com>
2020-04-06 04:30:27 +0900

    change to pandas and numpy

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import csv
+#from unittest.py import *
 
 def read_user_id():
     with open('input.txt', 'r') as f:
@@ -19,46 +20,78 @@ def do(ids):
     prediction = [['{},{},{}'.format(i, 5, 3.5)]*30 for i in ids]
     return prediction
 
-GENRES = 3
 def represent_movies():  
-    global genre_count, total_count, movie_table
-    with open('data/movies_w_imgurl.csv', 'r') as src:
-         reader = csv.reader(src)
-         # skip header 
-         next(reader) 
-         genre_count = dict()
-         total_count= 0 
-         for line in reader : 
-             total_count+=1
-             genres=line[GENRES].split('|')
-             for genre in genres:
-                 if genre_count.get(genre) == None:
-                    genre_count[genre] = 0
-                 genre_count[genre]+=1
-         # create IDF for genre_count 
-         TF = 1
-         for key in genre_count:
-             IDF = np.log10(total_count / genre_count[key])
-             genre_count[key] = TF * IDF
-         
-     
+    global genre_count, total_count, representation, movie_ids
+    df = pd.read_csv('data/movies_w_imgurl.csv', usecols=['movieId','genres'])
+    # set of genre
+    genres_list = set(df['genres'].tolist())
+    genre_count = { genre : 0 for genre in genres_list}
+    
+    for line in df['genres'].tolist(): 
+        items = line.split('|')
+        for item in items:
+            genre_count[item]+=1
+    
+    # check total count 
+    movie_ids = set(df['movieId'].tolist())
+    total_count = len(movie_ids)
+    print ("movie_count : "+str(total_count))
+    # create IDF for genre_count 
+    TF = 1
+    for key in genre_count.keys():
+        IDF = np.log10(total_count / genre_count[key])
+        genre_count[key] = TF * IDF
+    
+    # fill representation 
+    representation = df
+    for genre in genres_list:
+        representation[genre] = []  
 
-def check_genre():
-    with open('data/movies_w_imgurl.csv', 'r') as src:
-         reader = csv.reader(src)
-         # skip header 
-         next(reader) 
-         for line in reader : 
-             genres=line[GENRES].split('|')
-             out = "%s | "%line[0]
-             for genre in genres:
-                 out=out+"%s | "%genre_count[genre]  
-             print(out) 
+    for line in representation:
+        items = line['genre'].split('|')
+        for genre in genres_list:
+            if genre in items:
+               line[genre] = genre_count[genre]
+            else:
+               line[genre] = 0 
+            
+    print ("genre shape: %d" %(representation.shape))
+        
+        
+    
+
+def represent_tags():
+    global tag_count, total_count
+    df = pd.read_csv('data/tags.csv', usecols=['userId','movieId','tag','timestamp'])
+    tag_count = dict()
+    # count tags
+    for line in df['tag'].tolist():
+        tags = line.split(',').strip()
+        for tag in tags:
+            if tag_count.get(tag) == None:
+               tag_count[tag] = 0
+            tag_count[tag]+=1
+    # check tag 
+    print ("total tag : "+str(len(tag_count.keys())))
+    # calculate IDF for tag_count  
+    IDFs = dict()
+    for key in tag_count.keys():
+        IDFs[key] = np.log10(total_count / tag_count[key])
+    
+          
+         
+             
+
+           
+
+             
 
 if __name__ == "__main__":
     # task 1
     represent_movies()
-    check_genre()
+    #check_genre()
+    # task 2
+    
  
     # task 4 
     user_ids = read_user_id()
