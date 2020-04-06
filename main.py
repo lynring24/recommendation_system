f99234bd141a4eb4d71e12987484eaf36dea5e71
Herin Chung <lync2846@gmail.com>
2020-04-06 23:49:22 +0900

    getTF

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def do(ids):
 def represent_movies():  
     global genre_count, total_count, movie_ids
     df = pd.read_csv('data/movies_w_imgurl.csv', usecols = ['movieId','genres'])
-    #df = pd.read_csv('data/movies_w_imgurl.csv', names = ['movieId', 'id', 'movieName' ,'genres', 'URL'])
+    
     # set of genre
     genres_list = []
     for line in df['genres'].tolist():
@@ -30,33 +30,34 @@ def represent_movies():
     genres_list = set(genres_list)
     genre_count = { genre:0 for genre in iter(genres_list)}  
     
- 
+    # count genre 
     for line in df['genres'].tolist(): 
         items = line.split('|')
         for item in items:
             genre_count[item]+=1
+
     # check total count 
     movie_ids = set(df['movieId'].tolist())
     total_count = len(movie_ids)
-    print ("movie_count : "+str(total_count))
+
     # create IDF for genre_count 
     TF = 1
     for key in genre_count.keys():
         IDF = np.log10(total_count / genre_count[key])
         genre_count[key] = TF * IDF
-        print (key, genre_count[key])
     
     # fill representation 
     for genre in genres_list:
         df[genre] = 0.0 
 
-    print (df.shape)
+   
     for index,row in df.iterrows(): 
        	items = row['genres'].split('|')
         for genre in genres_list:
             if genre in items:
                df.at[index, genre] = genre_count[genre]
     store(df)
+    print (df.shape)
             
 
 
@@ -70,23 +71,37 @@ def store(table, fname=None):
        
 def represent_tags():
     global tag_count, total_count
-    df = pd.read_csv('data/tags.csv', usecols=['userId','movieId','tag','timestamp'])
-    tag_count = dict()
+    df_tag = pd.read_csv('data/tags.csv', usecols=['userId','movieId','tag','timestamp'])
+    
+    tag_list = []
+    for line in df_tag['tag'].tolist():
+        tag_list.extend(line.split(',').strip()) 
+    tag_list = set(tag_list)
+    tag_count = { tag:0 for tag in iter(tag_list)}
+
+    # init table for tag
+    for key in tag_list():
+        df_tag[key]=0.0
+
     # count tags
-    for index, row in df['tag'].tolist():
+    for row in df_tag['tag'].tolist():
         tags = row.split(',').strip()
         for tag in tags:
-            if tag_count.get(tag) == None:
-               tag_count[tag] = 0
             tag_count[tag]+=1
-    # check tag 
-    print ("total tag : "+str(len(tag_count.keys())))
-    # calculate IDF for tag_count  
+
+    # check total count 
+    movie_ids = set(df_tag['movieId'].tolist())
+    total_count = len(movie_ids) 
+             
+    # calculate IDF for tag  
     IDFs = dict()
     for key in tag_count.keys():
         IDFs[key] = np.log10(total_count / tag_count[key])
+   
+    # calacate TF for tag 
+     # read by movie with tags -> count total tags 
+     # iter by tag to clculate the TF
 
-             
 
 if __name__ == "__main__":
     # task 1
